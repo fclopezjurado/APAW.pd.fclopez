ac118a301d09b0ad830162f5abf30d20a3372a97
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 20:58:59 +0200

    New "es.upm.miw.pd.factoryMethod.naturalNumber.solution" test package and new "NaturalNumberManagerTest" class to test the new development.

---
+++ src/test/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumberManagerTest.java
@@ -0,0 +1,24 @@
+package es.upm.miw.pd.factoryMethod.naturalNumber.solution;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NaturalNumberManagerTest {
+
+	@Test
+	public void testSetNaturalNumberCreator() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNaturalNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNaturalNumber() {
+		fail("Not yet implemented");
+	}
+
+}
