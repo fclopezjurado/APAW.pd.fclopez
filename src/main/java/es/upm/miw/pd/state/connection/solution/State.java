3ae3552d18fb6f3adfd728e0780aa2eadcefebd0
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 18:11:44 +0200

    "State" class implementation in "es.upm.miw.pd.state.connection.solution" package

--- src/main/java/es/upm/miw/pd/state/connection/solution/State.java
+++ src/main/java/es/upm/miw/pd/state/connection/solution/State.java
@@ -2,13 +2,13 @@ package es.upm.miw.pd.state.connection.solution;
 
 public abstract class State {
 
-	public abstract void abrir();
+	public abstract void abrir(Conexion conexion);
 
-	public abstract void cerrar();
+	public abstract void cerrar(Conexion conexion);
 
-	public abstract void parar();
+	public abstract void parar(Conexion conexion);
 
-	public abstract void iniciar();
+	public abstract void iniciar(Conexion conexion);
 
 	public abstract void enviar(Conexion conexion, String msg);
 
