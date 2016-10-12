1f4a21b88cf344bdc390d2edcd74fc666703eb13
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 18:40:35 +0200

    "PreparadoState" class implementation in "es.upm.miw.pd.state.connection.solution" package

--- src/main/java/es/upm/miw/pd/state/connection/solution/PreparadoState.java
+++ src/main/java/es/upm/miw/pd/state/connection/solution/PreparadoState.java
@@ -1,5 +1,39 @@
 package es.upm.miw.pd.state.connection.solution;
 
-public class PreparadoState {
+public class PreparadoState extends State {
+
+	@Override
+	public void abrir(Conexion conexion) {
+	}
+
+	@Override
+	public void cerrar(Conexion conexion) {
+		conexion.setState(new CerradoState());
+	}
+
+	@Override
+	public void parar(Conexion conexion) {
+		conexion.setState(new ParadoState());
+	}
+
+	@Override
+	public void iniciar(Conexion conexion) {
+	}
+
+	@Override
+	public void enviar(Conexion conexion, String msg) {
+		conexion.getLink().enviar(msg);
+		conexion.setState(new EsperandoState());
+	}
+
+	@Override
+	public void recibir(Conexion conexion, int respuesta) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public Estado getEstado() {
+		return Estado.PREPARADO;
+	}
 
 }
