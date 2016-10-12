32d927a0d3e37b03e62502a69639cbeb0ea8df7e
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 20:24:35 +0200

    "NaturalNumber" class implementation in "es.upm.miw.pd.factoryMethod.naturalNumber.solution" package

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumber.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumber.java
@@ -1,5 +1,25 @@
 package es.upm.miw.pd.factoryMethod.naturalNumber.solution;
 
-public class NaturalNumber {
+public abstract class NaturalNumber {
+	private int value;
+
+	public NaturalNumber(int value) {
+		this.setValue(value);
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public void setValue(int value) {
+		assert value >= 0;
+		this.value = value;
+	}
+
+	public void add(int value) {
+		this.setValue(this.value + value);
+	}
+
+	public abstract String getTextValue();
 
 }
