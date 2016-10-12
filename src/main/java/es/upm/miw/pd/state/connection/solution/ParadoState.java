f7ac9b9cc2db497ee9d595305f0dc9c3eaaf6499
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 18:43:33 +0200

    "ParadoState" class implementation in "es.upm.miw.pd.state.connection.solution" package

--- src/main/java/es/upm/miw/pd/state/connection/solution/ParadoState.java
+++ src/main/java/es/upm/miw/pd/state/connection/solution/ParadoState.java
@@ -1,5 +1,39 @@
 package es.upm.miw.pd.state.connection.solution;
 
-public class ParadoState {
+public class ParadoState extends State {
+
+	@Override
+	public void abrir(Conexion conexion) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public void cerrar(Conexion conexion) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public void parar(Conexion conexion) {
+	}
+
+	@Override
+	public void iniciar(Conexion conexion) {
+		conexion.setState(new PreparadoState());
+	}
+
+	@Override
+	public void enviar(Conexion conexion, String msg) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public void recibir(Conexion conexion, int respuesta) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public Estado getEstado() {
+		return Estado.PARADO;
+	}
 
 }
