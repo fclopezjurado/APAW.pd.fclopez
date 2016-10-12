c3c2e5805ea5d23bcf89979c511e44ee32faa93c
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 13:32:43 +0200

    "Resta" class implementation in "es.upm.miw.pd.composite.expression.solution" package

--- src/main/java/es/upm/miw/pd/composite/expression/solution/Resta.java
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/Resta.java
@@ -1,5 +1,19 @@
 package es.upm.miw.pd.composite.expression.solution;
 
-public class Resta {
+public class Resta extends Operacion {
+
+	public Resta(Expresion operando1, Expresion operando2) {
+		super(operando1, operando2);
+	}
+
+	@Override
+	public float operar() {
+		return this.getOperando1().operar() - this.getOperando2().operar();
+	}
+
+	@Override
+	public String toString() {
+		return "(" + this.getOperando1().toString() + "-" + this.getOperando2().toString() + ")";
+	}
 
 }
