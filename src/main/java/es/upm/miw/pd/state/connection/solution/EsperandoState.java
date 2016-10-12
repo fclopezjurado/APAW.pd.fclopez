abdc59b887cd2529661f9b8766aab2b4d8d97af8
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 18:47:28 +0200

    "EsperandoState" class implementation in "es.upm.miw.pd.state.connection.solution" package

--- src/main/java/es/upm/miw/pd/state/connection/solution/EsperandoState.java
+++ src/main/java/es/upm/miw/pd/state/connection/solution/EsperandoState.java
@@ -1,5 +1,43 @@
 package es.upm.miw.pd.state.connection.solution;
 
-public class EsperandoState {
+public class EsperandoState extends State {
+
+	@Override
+	public void abrir(Conexion conexion) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public void cerrar(Conexion conexion) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public void parar(Conexion conexion) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public void iniciar(Conexion conexion) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public void enviar(Conexion conexion, String msg) {
+		throw new UnsupportedOperationException(State.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public void recibir(Conexion conexion, int respuesta) {
+		if (respuesta == 0)
+			conexion.setState(new PreparadoState());
+		else
+			conexion.setState(new CerradoState());
+	}
+
+	@Override
+	public Estado getEstado() {
+		return Estado.ESPERANDO;
+	}
 
 }
