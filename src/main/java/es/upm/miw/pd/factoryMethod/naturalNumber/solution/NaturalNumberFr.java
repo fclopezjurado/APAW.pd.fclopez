21e51f4496fac4dfc98eab89bfba52c63eb53064
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 20:38:08 +0200

    "NaturalNumberFr" class implementation in "es.upm.miw.pd.factoryMethod.naturalNumber.solution" package

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumberFr.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumberFr.java
@@ -1,5 +1,19 @@
 package es.upm.miw.pd.factoryMethod.naturalNumber.solution;
 
-public class NaturalNumberFr {
+public class NaturalNumberFr extends NaturalNumber {
+	private static final String[] textValue = { "zero", "un", "deux", "trois", "quatre", "cinq" };
+
+	public NaturalNumberFr(int value) {
+		super(value);
+	}
+
+	@Override
+	public String getTextValue() {
+		if (this.getValue() < textValue.length) {
+			return NaturalNumberFr.textValue[this.getValue()];
+		} else {
+			return "???";
+		}
+	}
 
 }
