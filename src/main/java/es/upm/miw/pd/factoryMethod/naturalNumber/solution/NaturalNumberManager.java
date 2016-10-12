772786038be2f8ec59897b8541950c7917391cca
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 20:42:46 +0200

    "NaturalNumberManager" class implementation in "es.upm.miw.pd.factoryMethod.naturalNumber.solution" package

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumberManager.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumberManager.java
@@ -1,5 +1,20 @@
 package es.upm.miw.pd.factoryMethod.naturalNumber.solution;
 
 public class NaturalNumberManager {
+	private NaturalNumber naturalNumber;
+
+	private NaturalNumberCreator naturalNumberCreator;
+
+	public void setNaturalNumberCreator(NaturalNumberCreator naturalNumberCreator) {
+		this.naturalNumberCreator = naturalNumberCreator;
+	}
+
+	public void createNaturalNumber() {
+		this.naturalNumber = this.naturalNumberCreator.createNaturalNumber();
+	}
+
+	public NaturalNumber getNaturalNumber() {
+		return this.naturalNumber;
+	}
 
 }
