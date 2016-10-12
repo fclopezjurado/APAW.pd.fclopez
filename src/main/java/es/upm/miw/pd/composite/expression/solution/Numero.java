f9798cffd3cbeaa5a4c1bfe41b4110bf5a17f356
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 13:07:38 +0200

    "Numero" class implementation in "es.upm.miw.pd.composite.expression.solution" package

--- src/main/java/es/upm/miw/pd/composite/expression/solution/Numero.java
+++ src/main/java/es/upm/miw/pd/composite/expression/solution/Numero.java
@@ -1,5 +1,24 @@
 package es.upm.miw.pd.composite.expression.solution;
 
-public class Numero {
+public class Numero extends Expresion {
+	private int valor;
+
+	public Numero(int valor) {
+		super();
+		this.setValor(valor);
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	private void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public float operar() {
+		return this.getValor();
+	}
 
 }
