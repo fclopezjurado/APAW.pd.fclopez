ad0554202bc0efb6129bb792e30380439b25d278
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-12 20:54:56 +0200

    "NaturalNumberFrCreator" class implementation in "es.upm.miw.pd.factoryMethod.naturalNumber.solution" package

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumberFrCreator.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/solution/NaturalNumberFrCreator.java
@@ -1,5 +1,10 @@
 package es.upm.miw.pd.factoryMethod.naturalNumber.solution;
 
-public class NaturalNumberFrCreator {
+public class NaturalNumberFrCreator extends NaturalNumberCreator {
+
+	@Override
+	public NaturalNumber createNaturalNumber(int value) {
+		return new NaturalNumberFr(value);
+	}
 
 }
