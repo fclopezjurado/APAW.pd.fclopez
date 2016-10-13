a8ad46da6754151d4561404c91e114d18f0b700b
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-14 00:09:30 +0200

    "FiguresManager" class implementation in "es.upm.miw.pd.visitor.figure.solution" package

--- src/main/java/es/upm/miw/pd/visitor/figure/solution/FiguresManager.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/solution/FiguresManager.java
@@ -1,5 +1,33 @@
 package es.upm.miw.pd.visitor.figure.solution;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FiguresManager {
+	private List<Figure> figures;
+
+	public FiguresManager() {
+		figures = new ArrayList<>();
+	}
+
+	public void add(Figure figure) {
+		figures.add(figure);
+	}
+
+	public double totalArea(Visitor visitor) {
+		double result = 0;
+		for (Figure figure : figures) {
+			result += figure.area(visitor);
+		}
+		return result;
+	}
+
+	public double totalNumberOfSides(Visitor visitor) {
+		double result = 0;
+		for (Figure figure : figures) {
+			result += figure.numberOfSides(visitor);
+		}
+		return result;
+	}
 
 }
