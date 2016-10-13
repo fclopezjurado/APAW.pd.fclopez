0caae725d1ee4d84921dbb676162b092f6ecb337
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-13 23:54:07 +0200

    "Visitor" abstract class implementation in "es.upm.miw.pd.visitor.figure.solution" package

--- src/main/java/es/upm/miw/pd/visitor/figure/solution/Visitor.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/solution/Visitor.java
@@ -1,5 +1,11 @@
 package es.upm.miw.pd.visitor.figure.solution;
 
-public class Visitor {
+public abstract class Visitor {
+
+	public abstract double visitCircle(Circle circle);
+
+	public abstract double visitSquare(Square square);
+
+	public abstract double visitTriangle(Triangle triangle);
 
 }
