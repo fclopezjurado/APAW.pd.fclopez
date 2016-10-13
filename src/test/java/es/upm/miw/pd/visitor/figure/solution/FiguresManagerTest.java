e0148902caba5f0ca7a02dfc714e4487f4970835
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-14 00:27:47 +0200

    New "es.upm.miw.pd.visitor.figure.solution" test package and new "FiguresManagerTest" class to test the new development.

---
+++ src/test/java/es/upm/miw/pd/visitor/figure/solution/FiguresManagerTest.java
@@ -0,0 +1,19 @@
+package es.upm.miw.pd.visitor.figure.solution;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FiguresManagerTest {
+
+	@Test
+	public void testTotalArea() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTotalNumberOfSides() {
+		fail("Not yet implemented");
+	}
+
+}
