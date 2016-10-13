b83741a712ff0b9ca9c8451c04a2741ec282c40a
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-13 23:44:23 +0200

    "Circle" class implementation in "es.upm.miw.pd.visitor.figure.solution" package

--- src/main/java/es/upm/miw/pd/visitor/figure/solution/Circle.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/solution/Circle.java
@@ -1,5 +1,25 @@
 package es.upm.miw.pd.visitor.figure.solution;
 
-public class Circle {
+public class Circle extends Figure {
+	private double radius;
+
+	public Circle(String description, double radius) {
+		super(description);
+		this.radius = radius;
+	}
+
+	public double getRadius() {
+		return this.radius;
+	}
+
+	@Override
+	public double area(Visitor visitor) {
+		return visitor.visitCircle(this);
+	}
+
+	@Override
+	public double numberOfSides(Visitor visitor) {
+		return visitor.visitCircle(this);
+	}
 
 }
