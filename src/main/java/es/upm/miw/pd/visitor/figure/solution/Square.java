284d45bb7d7b5a2b0bb1d264892fe1691fd4ccfa
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-13 23:46:40 +0200

    "Square" class implementation in "es.upm.miw.pd.visitor.figure.solution" package

--- src/main/java/es/upm/miw/pd/visitor/figure/solution/Square.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/solution/Square.java
@@ -1,5 +1,25 @@
 package es.upm.miw.pd.visitor.figure.solution;
 
-public class Square {
+public class Square extends Figure {
+	private double side;
+
+	public Square(String description, double side) {
+		super(description);
+		this.side = side;
+	}
+
+	public double getSide() {
+		return this.side;
+	}
+
+	@Override
+	public double area(Visitor visitor) {
+		return visitor.visitSquare(this);
+	}
+
+	@Override
+	public double numberOfSides(Visitor visitor) {
+		return visitor.visitSquare(this);
+	}
 
 }
