781259c7487d05533fe9216bdeab8da059221a8e
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-13 23:38:00 +0200

    "Figure" abstract class implementation in "es.upm.miw.pd.visitor.figure.solution" package

--- src/main/java/es/upm/miw/pd/visitor/figure/solution/Figure.java
+++ src/main/java/es/upm/miw/pd/visitor/figure/solution/Figure.java
@@ -1,5 +1,18 @@
 package es.upm.miw.pd.visitor.figure.solution;
 
-public class Figure {
+public abstract class Figure implements Element {
+	private String description;
+
+	public Figure(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public abstract double area(Visitor visitor);
+
+	public abstract double numberOfSides(Visitor visitor);
 
 }
