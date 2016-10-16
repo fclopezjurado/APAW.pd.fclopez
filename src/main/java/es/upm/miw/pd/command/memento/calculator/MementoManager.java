d5ff2fca0eb0338c09d6a57f7ebb1495c524645b
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 20:35:29 +0200

    "MementoManager" class implementation in "es.upm.miw.pd.command.memento.calculator" package

--- src/main/java/es/upm/miw/pd/command/memento/calculator/MementoManager.java
+++ src/main/java/es/upm/miw/pd/command/memento/calculator/MementoManager.java
@@ -1,5 +1,21 @@
 package es.upm.miw.pd.command.memento.calculator;
 
+import java.util.SortedMap;
+import java.util.TreeMap;
+
 public class MementoManager {
+	private SortedMap<String, Memento> lista = new TreeMap<String, Memento>();
+
+	public void addMemento(String key, Memento memento) {
+		this.lista.put(this.lista.size() + ":" + key, memento);
+	}
+
+	public Memento getMemento(String key) {
+		return this.lista.get(key);
+	}
+
+	public String[] keys() {
+		return this.lista.keySet().toArray(new String[0]);
+	}
 
 }
