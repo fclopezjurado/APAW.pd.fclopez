62d35a9c0b533c779ce7069fc6f0b070736ee9be
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 16:01:21 +0200

    "Componente" abstract class implementation in "es.upm.miw.pd.text.solution" package

--- src/main/java/es/upm/miw/pd/text/solution/Componente.java
+++ src/main/java/es/upm/miw/pd/text/solution/Componente.java
@@ -2,4 +2,16 @@ package es.upm.miw.pd.text.solution;
 
 public abstract class Componente {
 
+	public Componente() {
+
+	}
+
+	public abstract boolean remove(Componente componente);
+
+	public abstract void add(Componente componente);
+
+	public abstract boolean esCompuesto();
+
+	public abstract String dibujar(boolean enMayusculas);
+
 }
