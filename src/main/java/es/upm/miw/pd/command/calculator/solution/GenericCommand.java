6f1e65f6316119a25767af21f445f3f7e5a24574
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 19:14:39 +0200

    "GenericCommand" abstract class implementation in "es.upm.miw.pd.command.calculator.solution" package

--- src/main/java/es/upm/miw/pd/command/calculator/solution/GenericCommand.java
+++ src/main/java/es/upm/miw/pd/command/calculator/solution/GenericCommand.java
@@ -1,5 +1,18 @@
 package es.upm.miw.pd.command.calculator.solution;
 
-public abstract class GenericCommand {
+public abstract class GenericCommand extends AbstractCalculator implements Command {
+	private AbstractCalculator calculator;
+
+	public GenericCommand(AbstractCalculator calculator) {
+		this.calculator = calculator;
+	}
+
+	public AbstractCalculator getCalculator() {
+		return this.calculator;
+	}
+
+	public abstract void execute();
+
+	public abstract String name();
 
 }
