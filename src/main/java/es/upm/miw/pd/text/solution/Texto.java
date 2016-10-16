eceb0b10e5a4219e0cb76daa8e24c522a4d8d4f5
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 16:35:48 +0200

    "Texto" class implementation in "es.upm.miw.pd.text.solution" package

--- src/main/java/es/upm/miw/pd/text/solution/Texto.java
+++ src/main/java/es/upm/miw/pd/text/solution/Texto.java
@@ -1,5 +1,41 @@
 package es.upm.miw.pd.text.solution;
 
-public class Texto {
+public class Texto extends Capitulo {
+
+	public Texto() {
+		super();
+	}
+
+	@Override
+	public boolean remove(Componente componente) {
+		if (componente.esCompuesto())
+			return this.listaComponentes.remove(componente);
+		else
+			throw new UnsupportedOperationException(Parrafo.UNSUPPORTED_OPERATION_EXCEPTION);
+	}
+
+	@Override
+	public void add(Componente componente) {
+		if (componente.esCompuesto())
+			this.listaComponentes.add(componente);
+		else
+			throw new UnsupportedOperationException(Parrafo.UNSUPPORTED_OPERATION_EXCEPTION);
+
+	}
+
+	@Override
+	public boolean esCompuesto() {
+		return true;
+	}
+
+	@Override
+	public String dibujar(boolean enMayusculas) {
+		String texto = new String();
+
+		for (Componente componente : this.listaComponentes)
+			texto += componente.dibujar(enMayusculas);
+
+		return texto + "---o---\n";
+	}
 
 }
