1999481cc973a43970da1ec662a185e46604c4c5
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 19:09:59 +0200

    "AbstractCalculator" abstract class implementation in "es.upm.miw.pd.command.calculator.solution" package

--- src/main/java/es/upm/miw/pd/command/calculator/solution/AbstractCalculator.java
+++ src/main/java/es/upm/miw/pd/command/calculator/solution/AbstractCalculator.java
@@ -1,5 +1,30 @@
 package es.upm.miw.pd.command.calculator.solution;
 
 public abstract class AbstractCalculator {
+	protected int total;
+	
+	public AbstractCalculator() {
+        this.reset();
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    protected void setTotal(int total) {
+        this.total = total;
+    }
+
+    public void add(int valor) {
+        this.setTotal(this.total + valor);
+    }
+
+    public void subtract(int valor) {
+        this.setTotal(this.total - valor);
+    }
+
+    public void reset() {
+        this.setTotal(0);
+    }
 
 }
