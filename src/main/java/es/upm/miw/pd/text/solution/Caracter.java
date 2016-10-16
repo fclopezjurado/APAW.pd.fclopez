525e899780e339831ed187d3170d501d38763a05
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 16:07:59 +0200

    "Caracter" class implementation in "es.upm.miw.pd.text.solution" package

--- src/main/java/es/upm/miw/pd/text/solution/Caracter.java
+++ src/main/java/es/upm/miw/pd/text/solution/Caracter.java
@@ -1,5 +1,37 @@
 package es.upm.miw.pd.text.solution;
 
-public class Caracter {
+public class Caracter extends Componente {
+	private String caracter;
+
+	public Caracter(String caracter) {
+		this.setCaracter(caracter);
+	}
+
+	public String getCaracter() {
+		return this.caracter;
+	}
+
+	public void setCaracter(String caracter) {
+		this.caracter = caracter;
+	}
+
+	@Override
+	public boolean remove(Componente componente) {
+		return true;
+	}
+
+	@Override
+	public void add(Componente componente) {
+	}
+
+	@Override
+	public boolean esCompuesto() {
+		return false;
+	}
+
+	@Override
+	public String dibujar(boolean enMayusculas) {
+		return this.getCaracter();
+	}
 
 }
