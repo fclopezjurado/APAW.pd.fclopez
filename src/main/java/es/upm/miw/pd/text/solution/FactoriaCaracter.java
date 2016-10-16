ec829fad3eac318f7a43c5d415cd24aea33d449a
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 16:44:51 +0200

    "FactoriaCaracter" class implementation in "es.upm.miw.pd.text.solution" package

--- src/main/java/es/upm/miw/pd/text/solution/FactoriaCaracter.java
+++ src/main/java/es/upm/miw/pd/text/solution/FactoriaCaracter.java
@@ -1,5 +1,28 @@
 package es.upm.miw.pd.text.solution;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FactoriaCaracter {
+	private final Map<String, Caracter> coleccionCaracteres = new HashMap<String, Caracter>();
+	private static FactoriaCaracter factoriaCaracter = new FactoriaCaracter();
+
+	private FactoriaCaracter() {
+
+	}
+
+	public static FactoriaCaracter getFactoria() {
+		return FactoriaCaracter.factoriaCaracter;
+	}
+
+	public Caracter get(String key) {
+		if (this.coleccionCaracteres.containsKey(key))
+			return this.coleccionCaracteres.get(key);
+		else {
+			Caracter caracter = new Caracter(key);
+			this.coleccionCaracteres.put(key, caracter);
+			return caracter;
+		}
+	}
 
 }
