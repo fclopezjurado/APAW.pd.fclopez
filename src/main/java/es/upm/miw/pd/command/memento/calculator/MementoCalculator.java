4dec1813fd83236ad0fd91837fc7c6e029b7f62d
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 20:38:43 +0200

    "MementoCalculator" class implementation in "es.upm.miw.pd.command.memento.calculator" package

--- src/main/java/es/upm/miw/pd/command/memento/calculator/MementoCalculator.java
+++ src/main/java/es/upm/miw/pd/command/memento/calculator/MementoCalculator.java
@@ -1,5 +1,19 @@
 package es.upm.miw.pd.command.memento.calculator;
 
-public class MementoCalculator {
+import es.upm.miw.pd.command.calculator.solution.AbstractCalculator;
+
+public class MementoCalculator extends AbstractCalculator {
+
+	public MementoCalculator() {
+		super();
+	}
+
+	public Memento storeMemento() {
+		return new Memento(this.getTotal());
+	}
+
+	public void restoreMemento(Memento memento) {
+		this.setTotal(memento.getTotal());
+	}
 
 }
