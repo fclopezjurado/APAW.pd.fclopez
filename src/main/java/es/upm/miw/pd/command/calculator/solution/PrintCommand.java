6810f7726b99dfeb0276bcf0db1b6cb13bc962a1
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 19:32:06 +0200

    "PrintCommand" class implementation in "es.upm.miw.pd.command.calculator.solution" package

--- src/main/java/es/upm/miw/pd/command/calculator/solution/PrintCommand.java
+++ src/main/java/es/upm/miw/pd/command/calculator/solution/PrintCommand.java
@@ -1,5 +1,23 @@
 package es.upm.miw.pd.command.calculator.solution;
 
-public class PrintCommand {
+import upm.jbb.IO;
+
+public class PrintCommand extends GenericCommand {
+	private static String PRINT = "PRINT";
+
+	public PrintCommand(AbstractCalculator calculator) {
+		super(calculator);
+	}
+
+	@Override
+	public void execute() {
+		int total = this.getCalculator().getTotal();
+		IO.getIO().println(total);
+	}
+
+	@Override
+	public String name() {
+		return PrintCommand.PRINT;
+	}
 
 }
