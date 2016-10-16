3fbcaf9df796815bc398bc682bfa674fa5590b38
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 20:43:05 +0200

    "StoreCommand" class implementation in "es.upm.miw.pd.command.memento.calculator" package

--- src/main/java/es/upm/miw/pd/command/memento/calculator/StoreCommand.java
+++ src/main/java/es/upm/miw/pd/command/memento/calculator/StoreCommand.java
@@ -1,5 +1,31 @@
 package es.upm.miw.pd.command.memento.calculator;
 
-public class StoreCommand {
+import es.upm.miw.pd.command.calculator.solution.AbstractCalculator;
+import es.upm.miw.pd.command.calculator.solution.GenericCommand;
+import upm.jbb.IO;
+
+public class StoreCommand extends GenericCommand {
+	private static String STORE_MEMENTO = "STORE MEMENTO";
+	private MementoManager mementoManager;
+
+	public StoreCommand(AbstractCalculator calculator, MementoManager mementoManager) {
+		super(calculator);
+		this.mementoManager = mementoManager;
+	}
+
+	public MementoManager getMementoManager() {
+		return this.mementoManager;
+	}
+
+	@Override
+	public void execute() {
+		String mementoName = IO.getIO().readString();
+		this.mementoManager.addMemento(mementoName, new Memento(this.getCalculator().getTotal()));
+	}
+
+	@Override
+	public String name() {
+		return StoreCommand.STORE_MEMENTO;
+	}
 
 }
