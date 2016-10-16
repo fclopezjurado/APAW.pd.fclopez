9b29f1be96031e1d3a5d73c44f46aabb3ba49d64
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 19:28:28 +0200

    "SubtrackCommand" class implementation in "es.upm.miw.pd.command.calculator.solution" package

--- src/main/java/es/upm/miw/pd/command/calculator/solution/SubtractCommand.java
+++ src/main/java/es/upm/miw/pd/command/calculator/solution/SubtractCommand.java
@@ -1,5 +1,23 @@
 package es.upm.miw.pd.command.calculator.solution;
 
-public class SubtractCommand {
+import upm.jbb.IO;
+
+public class SubtractCommand extends GenericCommand {
+	private static String RESTA = "RESTA";
+
+	public SubtractCommand(AbstractCalculator calculator) {
+		super(calculator);
+	}
+
+	@Override
+	public void execute() {
+		int number = IO.getIO().readInt();
+		this.getCalculator().subtract(number);
+	}
+
+	@Override
+	public String name() {
+		return SubtractCommand.RESTA;
+	}
 
 }
