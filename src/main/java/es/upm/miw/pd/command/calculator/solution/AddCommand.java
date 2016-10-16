e2f4ce550c7dbdd463c94af01db0fc73bf3d844b
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 19:24:48 +0200

    "AddCommand" class implementation in "es.upm.miw.pd.command.calculator.solution" package

--- src/main/java/es/upm/miw/pd/command/calculator/solution/AddCommand.java
+++ src/main/java/es/upm/miw/pd/command/calculator/solution/AddCommand.java
@@ -1,5 +1,23 @@
 package es.upm.miw.pd.command.calculator.solution;
 
-public class AddCommand {
+import upm.jbb.IO;
+
+public class AddCommand extends GenericCommand {
+	private static String SUMA = "SUMA";
+
+	public AddCommand(AbstractCalculator calculator) {
+		super(calculator);
+	}
+
+	@Override
+	public void execute() {
+		int number = IO.getIO().readInt();
+        this.getCalculator().add(number);
+	}
+
+	@Override
+	public String name() {
+		return AddCommand.SUMA;
+	}
 
 }
