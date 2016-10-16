0f2faf3bda6a6721bd9fc3702539587f98ce1235
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-16 16:14:25 +0200

    "Capitulo" abstract class implementation in "es.upm.miw.pd.text.solution" package

--- src/main/java/es/upm/miw/pd/text/solution/Capitulo.java
+++ src/main/java/es/upm/miw/pd/text/solution/Capitulo.java
@@ -1,5 +1,21 @@
 package es.upm.miw.pd.text.solution;
 
-public abstract class Capitulo {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Capitulo extends Componente {
+	protected List<Componente> listaComponentes;
+
+	public Capitulo() {
+		this.listaComponentes = new ArrayList<Componente>();
+	}
+
+	public abstract boolean remove(Componente componente);
+
+	public abstract void add(Componente componente);
+
+	public abstract boolean esCompuesto();
+
+	public abstract String dibujar(boolean enMayusculas);
 
 }
