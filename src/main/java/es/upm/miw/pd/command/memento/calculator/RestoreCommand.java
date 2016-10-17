3ded897ecfb1e7394e1617d986d1b99303fa6fa0
franlopez <franlopez@DESKTOP-FCHO9F1>
2016-10-17 20:55:59 +0200

    "RestoreCommand" class implementation in "es.upm.miw.pd.command.memento.calculator" package

--- src/main/java/es/upm/miw/pd/command/memento/calculator/RestoreCommand.java
+++ src/main/java/es/upm/miw/pd/command/memento/calculator/RestoreCommand.java
@@ -1,5 +1,23 @@
 package es.upm.miw.pd.command.memento.calculator;
 
-public class RestoreCommand {
+import upm.jbb.IO;
+
+public class RestoreCommand extends MementableGenericCommand {
+	private static String RESTORE_MEMENTO = "RESTORE MEMENTO";
+
+	public RestoreCommand(MementoCalculator calculator) {
+		super(calculator);
+	}
+
+	@Override
+	public void execute() {
+		String mementoKey = (String) IO.getIO().select(this.getMementoCalculator().getMementoManager().keys());
+		this.getMementoCalculator().restoreMemento(mementoKey);
+	}
+
+	@Override
+	public String name() {
+		return RestoreCommand.RESTORE_MEMENTO;
+	}
 
 }
